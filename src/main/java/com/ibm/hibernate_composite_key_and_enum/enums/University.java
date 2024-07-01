8eab429c0b3cecadb97d3ef7bcc74e0293a31a31
Mohammed-Masood-Ansari <masoodansari1997@gmail.com>
2024-07-01 19:37:48 +0530

    enum wala code

---
+++ src/main/java/com/ibm/hibernate_composite_key_and_enum/enums/University.java
@@ -0,0 +1,9 @@
+package com.ibm.hibernate_composite_key_and_enum.enums;
+
+public enum University {
+
+	VTU,
+	BBD,
+	DU,
+	MSR
+}
---
+++ src/main/java/com/ibm/hibernate_composite_key_and_enum/enums/UniversityDriver.java
@@ -0,0 +1,12 @@
+package com.ibm.hibernate_composite_key_and_enum.enums;
+
+public class UniversityDriver {
+
+	public static void main(String[] args) {
+		
+		University university = University.BBD;
+		
+		System.out.println(university);
+		
+	}
+}
